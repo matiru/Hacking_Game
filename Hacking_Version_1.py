5d7bb716ef2446a66c86e27da70da6d292cb41fc
matiru <gemmatiru@gmail.com>
2024-02-06 11:49:26 +0300

    Hacking_version_1

---
+++ Hacking_Version_1.py
@@ -0,0 +1,50 @@
+# Hacking version 1
+# This is a text-based password guessing game that displays
+# a list of potential computer password.The player is allowed 1 attempt
+# The game indicates that the player failed to guess the password correctly.
+
+
+# display header
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+# display password list
+
+print('PROVIDE\nSETTING\nCANTINA\nCUTTING\nHUNTERS\nSURVUVE\nHUNTING\nREALIZE\nNOTHING\nOVERLAP\nFINDING\nPUTTING')
+
+#print('PROVIDE')
+#print('SETTING')
+#print('CANTINA')
+#print('CUTTING')
+#print('HUNTERS')
+#print('SURVIVE')
+#print('HEARING')
+#print('HUNTING')
+#print('REALIZE')
+#print('NOTHING')
+#print('OVERLAP')
+#print('FINDING')
+#print('PUTTING')
+print('')
+
+# prompt for guess
+input('Enter a password >')
+
+# end game
+#     display failure outcome
+print('LOGIN FAILURE - TERMINAL LOCKED')
+print('')
+print('PLEASE CONTACT AN ADMINISTRATOR')
+print('')
+#    prompt for end
+print('PRESS ENTER TO EXIT')
+
+#1
+#1.1 Program Comment
+#1.1.1 Is there one at the start of the program?
+
+#1.1.2 Does it indicate what the program does?
+
+#1.2 Block Comment (comment on its own line)
+#1.2.1 Is there one at the start of each group of statements that completes a logical task?
